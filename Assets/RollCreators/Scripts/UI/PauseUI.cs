1a2d9ac62c1ec336404328c16c8c30c71ed4b440
zhuravlev12 <slawajur@mail.ru>
2020-06-28 20:20:24 +0700

    fixed bugs

--- Assets/RollCreators/Scripts/UI/PauseUI.cs
+++ Assets/RollCreators/Scripts/UI/PauseUI.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseUI : MonoBehaviour
 {
     [SerializeField] private Game game;
     [SerializeField] private AcknowledgementUI ack;
+    [SerializeField] private Text soundText;
 
     public void Show()
     {
         gameObject.SetActive(true);
         game.isPaused = true;
+        UpdateSoundButton();
     }
 
     public void Continue()
@@ -22,5 +25,30 @@ public class PauseUI : MonoBehaviour
     public void ExitGame()
     {
         ack.Show();
+    }
+
+    public void SwitchSound()
+    {
+        if (AudioListener.volume == 0)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+        }
+        UpdateSoundButton();
+    }
+
+    private void UpdateSoundButton()
+    {
+        if (AudioListener.volume == 0)
+        {
+            soundText.text = "Sound on";
+        }
+        else
+        {
+            soundText.text = "Sound off";
+        }
     }
 }
